afcf56345299915ebba33dc038578ede2fd14127
Multiweb-Guilherme <Multiweb-Guilherme@172.16.0.60>
2013-01-15 12:55:32 -0200

    tentando

--- src/estacionamentojg/EstacionamentoJG.java
+++ src/estacionamentojg/EstacionamentoJG.java
@@ -5,11 +5,6 @@ public class EstacionamentoJG {
 
     
     public static void main(String[] args) {
-        //commit feito
-        /*testando agora
-         * pra ver se funciona o envio de commit
-         */
-        //test commit novo
-        //teste teste teste
+        
     }
 }
